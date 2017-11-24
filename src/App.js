80018a56380fc9e04bebc549abac9e1fc2990ba1
chapuzzo <luismax@gmail.com>
2017-11-24 23:34:25 +0100

    removed all vue traces

---
+++ src/App.js
@@ -0,0 +1,5 @@
+export default class {
+  doSomething () {
+    console.log('yeah!')
+  }
+}
--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -4,22 +4,19 @@ module.exports = {
   rootDir: path.resolve(__dirname, '../../'),
   moduleFileExtensions: [
     'js',
-    'json',
-    'vue'
+    'json'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest'
   },
-  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
   mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
-    'src/**/*.{js,vue}',
+    'src/**/*.{js}',
     '!src/main.js',
     '!src/router/index.js',
     '!**/node_modules/**'
